361abcf0a9729688c515587fdbf11fff44902f5a
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2021-03-30 07:24:37 +0700

    fix heroku

---
+++ datas/sizes.js
@@ -0,0 +1,19 @@
+const sizes = [
+    { id: 0, width: 1000, height: 1000 },
+    { id: 1, width: 150, height: 600 },
+    { id: 2, width: 150, height: 800 },
+    { id: 3, width: 300, height: 300 },
+    { id: 4, width: 400, height: 400 },
+    { id: 5, width: 400, height: 800 },
+    { id: 6, width: 500, height: 500 },
+    { id: 7, width: 600, height: 1200 },
+    { id: 8, width: 600, height: 600 },
+    { id: 9, width: 600, height: 900 },
+    { id: 10, width: 800, height: 150 },
+    { id: 11, width: 800, height: 800 },
+    { id: 12, width: 600, height: 300 },
+    { id: 13, width: 300, height: 450 },
+    { id: 14, width: 350, height: 450 },
+    { id: 15, width: 800, height: 400 },
+];
+export default sizes
---
+++ datas/zfronts.js
@@ -0,0 +1,7 @@
+const fronts=[
+    { id: 1, name: 'Bóng' },
+    { id: 2, name: 'Matt' },
+    { id: 3, name: 'Mài lướt' },
+    { id: 4, name: 'Dị Hình' },
+];
+export default fronts
