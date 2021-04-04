19f4a506f0bc103ebe0559a69ae39714d31a3877
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2021-04-04 21:47:48 +0700

    for dev2

--- utils/helper.js
+++ utils/helper.js
@@ -1,7 +1,8 @@
 export const PI = Math.PI;
+export const deg = (a) => a * PI / 180;
 export const sin = Math.sin;
-export const sinD = (a) => Math.sin(a * PI / 180);
+export const sinD = (a) => Math.sin(deg(a));
 export const cos = Math.cos;
-export const cosD = (a) => Math.cos(a * PI / 180);
+export const cosD = (a) => Math.cos(deg(a));
 export const tan = Math.tan;
-export const tanD = (a) => Math.tan(a * PI / 180);
+export const tanD = (a) => Math.tan(deg(a));
