c421decefaea5549e435dfc3b0ad7ce9855ce1bf
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2021-05-10 20:08:53 +0700

    test mongo

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 module.exports = {
     env: {
         // HOST_NAME: 'localhost',
-        HOST_NAME: 'heroku',
-        // HOST_NAME: 'production',
+        // HOST_NAME: 'heroku',
+        HOST_NAME: 'production',
 
-        MONGODB_URL: "mongodb+srv://Trang8:AdminTrang8@trang8-cepg4.mongodb.net/visualizer?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true",
-        // MONGODB_URL: "mongodb://localhost:27017/visualizer",
+        // MONGODB_URL: "mongodb+srv://Trang8:AdminTrang8@trang8-cepg4.mongodb.net/visualizer?retryWrites=true&w=majority&useNewUrlParser=true&useUnifiedTopology=true",
+        MONGODB_URL: "mongodb://localhost:27017/visualizer",
 
         // API_URL: 'https://reallyvirtual.herokuapp.com/api/',
-        API_URL: 'http://localhost:3000/api/',
+        // API_URL: 'http://localhost:3000/api/',
         // API_URL: 'http://demo.unicreation.net/api/',
+
+        API_URL: 'http://103.159.52.27:3000/',
 
         FOLDER_UPLOAD: 'upload',
 
--- pages/api/images/[name].js
+++ pages/api/images/[name].js
@@ -1,15 +1,28 @@
-const http = require('http');
-const fs = require('fs');
+import http from 'http';
+import path from 'path';
+import sharp from 'sharp';
+import fs from 'fs';
 
 export default async function images(req, res) {
     const { width, height, name } = req.query;
     if (process.env.HOST_NAME == 'production' || process.env.HOST_NAME == 'localhost') {
         try {
             const file = fs.readFileSync(process.env.FOLDER_UPLOAD + "/" + name);
-            return res.end(file, 'binary');
+            res.writeHead(200, { 'Content-Type': 'image/jpeg' });
+            if (Number(width) && Number(height)) {
+                const thumbnailFolder = `./${process.env.FOLDER_UPLOAD}/thumbnail/`;
+                const cropedPath = `${thumbnailFolder + path.basename(name, path.extname(name))}-${width}-${height + path.extname(name)}`;
+                if (!fs.existsSync(cropedPath)) {
+                    if (!fs.existsSync(thumbnailFolder)) fs.mkdirSync(thumbnailFolder);
+                    await sharp(file).resize(Number(width), Number(height)).toFile(cropedPath);
+                }
+                const imageCroped = fs.readFileSync(cropedPath);
+                return res.end(imageCroped);
+            }
+            return res.end(file);
         }
-        catch {
-            res.status(404).send();
+        catch (e) {
+            return res.status(404).send();
         }
     }
     return new Promise(resolve => {
