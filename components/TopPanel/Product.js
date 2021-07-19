24c07758fcf997c680515c365345be6755f4051d
ngocdrakula@gmail.com <ngocdrakula@gmail.com>
2021-07-19 14:23:45 +0700

    add load more for product image

--- components/TopPanel/Product.js
+++ components/TopPanel/Product.js
@@ -3,13 +3,23 @@ import { connect } from 'react-redux';
 import types from '../../redux/types';
 import { getThumbnail } from '../../utils/helper';
 
+const unitHeight = 100.8, groupLength = 100;
+
 class Product extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            limit: groupLength
         }
     }
 
+    handleScroll = (e) => {
+        const { scrollTop } = e.target;
+        const limit = (Math.floor(scrollTop / unitHeight / groupLength) + 1) * groupLength;
+        if (limit != this.state.limit) {
+            this.setState({ limit })
+        }
+    }
     render() {
         const { dispatch, products, layout, areaIndex, sizes, fronts, search } = this.props;
         const area = layout?.areas?.[areaIndex] || {};
@@ -22,9 +32,10 @@ class Product extends Component {
             if (!product.room.includes(layout?.room?._id)) return false;
             if (!product.type.includes(type)) return false;
             return true;
-        })
+        });
+        const { limit } = this.state;
         return (
-            <div id="topPanelTilesListBox" className="top-panel-box">
+            <div id="topPanelTilesListBox" className="top-panel-box" onScroll={this.handleScroll}>
                 <div id="loadTilesAnimationContainer" style={products.length ? { display: 'none' } : {}}>
                     <p>Đang tải</p>
                     <div className="circles marginLeft">
@@ -34,12 +45,12 @@ class Product extends Component {
                     </div>
                 </div>
                 <ul>
-                    {sortedProducts.map(product => {
+                    {sortedProducts.map((product, index) => {
                         const disabled = !!(selecteds.findIndex(p => p.size.width === product.size.width && p.size.height === product.size.height));
                         return (
                             <li key={product._id} className="top-panel-content-tiles-list-item"  >
                                 <div className="tile-list-thumbnail-image-holder" onClick={() => dispatch({ type: types.SELECT_PRODUCT, payload: product })}>
-                                    <img src={"/api/images/" + getThumbnail(product)} />
+                                    <img src={index < limit ? "/api/images/" + getThumbnail(product) : undefined} />
                                 </div>
                                 <div className="tile-list-text" onClick={() => dispatch({ type: types.SELECT_PRODUCT, payload: product })}>
                                     <p className="-caption">{product.name}</p>
